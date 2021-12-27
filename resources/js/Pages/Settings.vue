116a008c00cf62377d906ce3a87bbc5e363df317
Thanos <stantzouris.thanos@gmail.com>
2021-12-27 12:58:43 +0200

    LC Class 13: Default Layouts

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -2,11 +2,3 @@
     <h1 class='text-4xl font-bold'>Settings!</h1>
 </template>
 
-<script>
-
-    import Layout from '../Shared/Layout';
-
-    export default {
-        layout: Layout,
-    };
-</script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -2,11 +2,3 @@
     <h1 class='text-4xl font-bold'>Users!</h1>
 </template>
 
-<script>
-
-    import Layout from '../Shared/Layout';
-
-    export default {
-        layout: Layout,
-    };
-</script>
